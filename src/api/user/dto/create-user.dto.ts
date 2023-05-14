e3350870ce1e9e9fc79b430ac7b4c328bae98439
valoon3 <valoon2@naver.com>
2023-05-14 19:51:35 +0900

    our-trip nest 로그인 구현 시작

---
+++ src/api/user/dto/create-user.dto.ts
@@ -0,0 +1,5 @@
+export class CreateUserDto {
+  userEmail: string;
+  password: string;
+  name?: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
   const port = 8080;
 
   await app.listen(port).then(() => {
     console.log(`waiting in ${port}!!!`);
   });
 }
+
 bootstrap();
