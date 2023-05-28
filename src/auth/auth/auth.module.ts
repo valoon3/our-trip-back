03f00aab6b3633fe5ef9d940c26a4abc3c892aa8
valoon3 <valoon2@naver.com>
2023-05-28 22:29:12 +0900

    our-trip nest jwt passport 전략 오류 수정 완료
    1. 오류 수정 완료
    2. 하지만 일단은 주석 추가 필요하다.

--- src/auth/auth/auth.module.ts
+++ src/auth/auth/auth.module.ts
@@ -1,17 +1,18 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserModule } from '../../api/user/user.module';
-import { UserRepository } from '../../api/user/user.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
-    forwardRef(() => UserModule),
+    PassportModule.register({ defaultStrategy: 'jwt', session: false }),
     JwtModule.register({
       secret: 'secret',
       signOptions: { expiresIn: '7d' },
     }),
+    forwardRef(() => UserModule),
   ],
   providers: [AuthService, JwtStrategy],
   exports: [AuthService],
--- src/auth/auth/jwt/jwt.strategy.ts
+++ src/auth/auth/jwt/jwt.strategy.ts
@@ -1,10 +1,11 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { UserRepository } from '../../../api/user/user.repository';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly catRepositroy) {
+  constructor(private readonly userRepositroy: UserRepository) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // JWT 가 Req 에서 추출되는 방법을 제공 // 스키마 bearer 를 사용하여 인증 헤더에서 JWT 를 찾는다
       secretOrKey: 'secret', // 토큰 설명을 위해 대칭 비밀키를 제공하는 옵션
@@ -13,14 +14,14 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload) {
-    const cat = await this.catRepositroy.findByCatIdWithoutPassword(
+    const user = await this.catRepositroy.findByCatIdWithoutPassword(
       payload.sub,
     );
 
-    if (!cat) {
-      throw new UnauthorizedException('고양이가 존재하지 않습니다.');
+    if (!user) {
+      throw new UnauthorizedException('사용자가 존재하지 않습니다.');
     }
 
-    return cat;
+    return user;
   }
 }
