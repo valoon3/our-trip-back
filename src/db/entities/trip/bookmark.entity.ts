a44d1e479838f6d01b7700bf93b26acc28e8938c
valoon3 <valoon2@naver.com>
2023-11-30 17:36:15 +0900

    <feat> swagger bookmark setting

--- src/db/entities/trip/bookmark.entity.ts
+++ src/db/entities/trip/bookmark.entity.ts
@@ -7,10 +7,15 @@ import {
 } from 'typeorm';
 import { User } from '../User.entity';
 import { Place } from './Place.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'bookmarks' })
 export class Bookmark {
   @PrimaryGeneratedColumn()
+  @ApiProperty({
+    description: '기본키',
+    default: 1,
+  })
   id: number;
 
   // @JoinColumn({ name: 'place_id' })
@@ -19,8 +24,16 @@ export class Bookmark {
   @Index()
   @ManyToOne(() => User, (User) => User.id)
   @JoinColumn({ name: 'user_id' })
+  @ApiProperty({
+    description: 'user_id',
+    default: 1,
+  })
   user: User;
 
   @ManyToOne(() => Place, (Place) => Place.id)
+  @ApiProperty({
+    description: 'place_id',
+    default: 1,
+  })
   place: Place;
 }
