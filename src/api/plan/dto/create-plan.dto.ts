76600c5266eaaaa9cc6f13d1f8d11d15b3163c61
valoon3 <valoon2@naver.com>
2024-01-02 14:49:20 +0900

    <fix> create-plan.dto 수정

--- src/api/plan/dto/create-plan.dto.ts
+++ src/api/plan/dto/create-plan.dto.ts
@@ -2,6 +2,7 @@ import { GoogleMapPlaceResult } from '../../../common/types/googleMap.type';
 
 export class CreatePlanDto extends GoogleMapPlaceResult {
   title: string;
-  startPlanDate?: Date;
-  endPlanDate?: Date;
+  description?: string;
+  startDate: Date;
+  endDate: Date;
 }
