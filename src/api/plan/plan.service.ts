c3a2a6f6d7ad680b9d9f673aeab0e6c29f6e029a
valoon3 <valoon2@naver.com>
2024-01-05 16:57:46 +0900

    <fix> entity 변수명 수정
    1. 생성일자, 수정일자 추가

--- src/api/plan/plan.service.ts
+++ src/api/plan/plan.service.ts
@@ -6,6 +6,7 @@ import { UserRepository } from '../user/user.repository';
 import { PlaceRepository } from '../trip/place.repository';
 import { Plan } from '../../db/entities/trip/plan.entity';
 import { PlanDetailRepository } from './planDetail.repository';
+import { PlanDto } from './dto/plan.dto';
 
 @Injectable()
 export class PlanService {
@@ -18,9 +19,9 @@ export class PlanService {
 
   // 계획 생성
   async createPlan(user: any, createPlanDto: CreatePlanDto) {
-    const plans = await this.findAllPlan(user);
-    const exist = plans.find((plan) => plan.title === createPlanDto.title);
-
+    // const plans = await this.findAllPlan(user);
+    // const exist = plans.find((plan) => plan.title === createPlanDto.title);
+    //
     // if (exist)
     //   return {
     //     message: '이미 존재하는 계획입니다.',
@@ -38,15 +39,24 @@ export class PlanService {
 
   // 해당 사용자의 모든 계획을 가져온다.
   async findAllPlan(user) {
-    let result;
     try {
-      result = await this.planRepository.find({
+      const planList: Plan[] = await this.planRepository.find({
         where: { user },
       });
+
+      const result = planList.map((plan) => ({
+        title: plan.title,
+        description: plan.description,
+        startDate: plan.startDate,
+        endDate: plan.endDate,
+        createdAt: plan.createdAt,
+        updatedAt: plan.updatedAt,
+      }));
+
+      return result;
     } catch (err) {
       console.error(err);
     }
-    return result;
   }
 
   findOne(id: number) {
